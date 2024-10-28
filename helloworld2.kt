d90883c191aa061a51aec5dbbb0bcc5c695caffd
pablo <pablomolina@gmail.com>
2024-10-28 09:50:50 +0100

    Resuelve conflicto

--- helloworld2.kt
+++ helloworld2.kt
@@ -1,8 +1,3 @@
 println("Hola Mundo 2")
 println("Modificacion de fichero")
 println("Modificacion para MERGE")
-<<<<<<< HEAD
-println("Modificacion para provocar un conflicto")
-=======
-println("Modificacion 2")
->>>>>>> main
